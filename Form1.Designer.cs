0b06c198554b893912af05b63eb085a3ac91d2bd
negri <negri@DESKTOP-ACE46T4>
2025-05-30 22:26:17 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace cocstart
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            playbutton = new Button();
+            pictureBox1 = new PictureBox();
+            themebutton = new Button();
+            freemodebutton = new Button();
+            statsbutton = new Button();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            gear = new PictureBox();
+            pictureBox7 = new PictureBox();
+            quit = new Button();
+            pictureBox6 = new PictureBox();
+            togglemusicoff = new PictureBox();
+            togglemusicon = new PictureBox();
+            pictureBox8 = new PictureBox();
+            info = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)gear).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)togglemusicoff).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)togglemusicon).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            SuspendLayout();
+            // 
+            // playbutton
+            // 
+            playbutton.BackColor = Color.Azure;
+            playbutton.BackgroundImageLayout = ImageLayout.Center;
+            playbutton.CausesValidation = false;
+            playbutton.FlatAppearance.BorderSize = 0;
+            playbutton.FlatAppearance.MouseOverBackColor = Color.Azure;
+            playbutton.FlatStyle = FlatStyle.Flat;
+            playbutton.Font = new Font("Microsoft JhengHei", 25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            playbutton.ForeColor = Color.White;
+            playbutton.Location = new Point(737, 313);
+            playbutton.Margin = new Padding(3, 2, 3, 2);
+            playbutton.Name = "playbutton";
+            playbutton.Size = new Size(213, 52);
+            playbutton.TabIndex = 0;
+            playbutton.Text = "PLAY";
+            playbutton.UseVisualStyleBackColor = false;
+            playbutton.Click += playbutton_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(643, 259);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(398, 157);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // themebutton
+            // 
+            themebutton.BackColor = Color.Azure;
+            themebutton.BackgroundImageLayout = ImageLayout.Center;
+            themebutton.FlatAppearance.BorderSize = 0;
+            themebutton.FlatAppearance.MouseOverBackColor = Color.Azure;
+            themebutton.FlatStyle = FlatStyle.Flat;
+            themebutton.Font = new Font("Microsoft JhengHei", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            themebutton.ForeColor = Color.White;
+            themebutton.Location = new Point(750, 560);
+            themebutton.Margin = new Padding(3, 2, 3, 2);
+            themebutton.Name = "themebutton";
+            themebutton.Size = new Size(150, 40);
+            themebutton.TabIndex = 2;
+            themebutton.Text = "Themes";
+            themebutton.UseVisualStyleBackColor = false;
+            themebutton.Click += themebutton_Click;
+            // 
+            // freemodebutton
+            // 
+            freemodebutton.BackColor = Color.Azure;
+            freemodebutton.BackgroundImageLayout = ImageLayout.Center;
+            freemodebutton.FlatAppearance.BorderSize = 0;
+            freemodebutton.FlatAppearance.MouseOverBackColor = Color.Azure;
+            freemodebutton.FlatStyle = FlatStyle.Flat;
+            freemodebutton.Font = new Font("Microsoft JhengHei", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            freemodebutton.ForeColor = Color.White;
+            freemodebutton.Location = new Point(750, 458);
+            freemodebutton.Margin = new Padding(3, 2, 3, 2);
+            freemodebutton.Name = "freemodebutton";
+            freemodebutton.Size = new Size(150, 40);
+            freemodebutton.TabIndex = 3;
+            freemodebutton.Text = "Free Mode";
+            freemodebutton.UseVisualStyleBackColor = false;
+            freemodebutton.Click += freemodebutton_Click;
+            // 
+            // statsbutton
+            // 
+            statsbutton.BackColor = Color.Azure;
+            statsbutton.BackgroundImageLayout = ImageLayout.Center;
+            statsbutton.FlatAppearance.BorderSize = 0;
+            statsbutton.FlatAppearance.MouseOverBackColor = Color.Azure;
+            statsbutton.FlatStyle = FlatStyle.Flat;
+            statsbutton.Font = new Font("Microsoft JhengHei", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            statsbutton.ForeColor = Color.White;
+            statsbutton.Location = new Point(750, 669);
+            statsbutton.Margin = new Padding(3, 2, 3, 2);
+            statsbutton.Name = "statsbutton";
+            statsbutton.Size = new Size(150, 40);
+            statsbutton.TabIndex = 4;
+            statsbutton.Text = "Statistics";
+            statsbutton.UseVisualStyleBackColor = false;
+            statsbutton.Click += statsbutton_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(704, 420);
+            pictureBox2.Margin = new Padding(3, 2, 3, 2);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(263, 105);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.Transparent;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(704, 530);
+            pictureBox3.Margin = new Padding(3, 2, 3, 2);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(263, 105);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 6;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.Transparent;
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(704, 639);
+            pictureBox4.Margin = new Padding(3, 2, 3, 2);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(263, 105);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 7;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = Color.Transparent;
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(10, 695);
+            pictureBox5.Margin = new Padding(3, 2, 3, 2);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(100, 70);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 8;
+            pictureBox5.TabStop = false;
+            // 
+            // gear
+            // 
+            gear.BackColor = Color.Azure;
+            gear.Image = (Image)resources.GetObject("gear.Image");
+            gear.Location = new Point(43, 714);
+            gear.Margin = new Padding(3, 2, 3, 2);
+            gear.Name = "gear";
+            gear.Size = new Size(31, 26);
+            gear.SizeMode = PictureBoxSizeMode.Zoom;
+            gear.TabIndex = 9;
+            gear.TabStop = false;
+            gear.Click += gear_Click;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.BackColor = Color.Transparent;
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(1554, 9);
+            pictureBox7.Margin = new Padding(3, 2, 3, 2);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(100, 70);
+            pictureBox7.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox7.TabIndex = 10;
+            pictureBox7.TabStop = false;
+            // 
+            // quit
+            // 
+            quit.BackColor = Color.Azure;
+            quit.BackgroundImageLayout = ImageLayout.Center;
+            quit.CausesValidation = false;
+            quit.FlatAppearance.BorderSize = 0;
+            quit.FlatAppearance.MouseOverBackColor = Color.Azure;
+            quit.FlatStyle = FlatStyle.Flat;
+            quit.Font = new Font("Microsoft JhengHei", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            quit.ForeColor = Color.White;
+            quit.Location = new Point(1586, 32);
+            quit.Margin = new Padding(3, 2, 3, 2);
+            quit.Name = "quit";
+            quit.Size = new Size(31, 26);
+            quit.TabIndex = 11;
+            quit.Text = "X";
+            quit.UseVisualStyleBackColor = false;
+            quit.Click += quit_Click;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = Color.Transparent;
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(1554, 695);
+            pictureBox6.Margin = new Padding(3, 2, 3, 2);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(100, 70);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 12;
+            pictureBox6.TabStop = false;
+            // 
+            // togglemusicoff
+            // 
+            togglemusicoff.BackColor = Color.Azure;
+            togglemusicoff.Image = (Image)resources.GetObject("togglemusicoff.Image");
+            togglemusicoff.Location = new Point(1586, 718);
+            togglemusicoff.Margin = new Padding(3, 2, 3, 2);
+            togglemusicoff.Name = "togglemusicoff";
+            togglemusicoff.Size = new Size(31, 26);
+            togglemusicoff.SizeMode = PictureBoxSizeMode.Zoom;
+            togglemusicoff.TabIndex = 13;
+            togglemusicoff.TabStop = false;
+            togglemusicoff.Click += togglemusic_Click;
+            // 
+            // togglemusicon
+            // 
+            togglemusicon.BackColor = Color.Azure;
+            togglemusicon.Image = (Image)resources.GetObject("togglemusicon.Image");
+            togglemusicon.Location = new Point(1517, 718);
+            togglemusicon.Margin = new Padding(3, 2, 3, 2);
+            togglemusicon.Name = "togglemusicon";
+            togglemusicon.Size = new Size(31, 26);
+            togglemusicon.SizeMode = PictureBoxSizeMode.Zoom;
+            togglemusicon.TabIndex = 14;
+            togglemusicon.TabStop = false;
+            togglemusicon.Click += togglemusicon_Click;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.BackColor = Color.Transparent;
+            pictureBox8.Image = (Image)resources.GetObject("pictureBox8.Image");
+            pictureBox8.Location = new Point(10, 9);
+            pictureBox8.Margin = new Padding(3, 2, 3, 2);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(100, 70);
+            pictureBox8.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox8.TabIndex = 15;
+            pictureBox8.TabStop = false;
+            // 
+            // info
+            // 
+            info.BackColor = Color.Azure;
+            info.BackgroundImageLayout = ImageLayout.Center;
+            info.CausesValidation = false;
+            info.FlatAppearance.BorderSize = 0;
+            info.FlatAppearance.MouseOverBackColor = Color.Azure;
+            info.FlatStyle = FlatStyle.Flat;
+            info.Font = new Font("Microsoft JhengHei", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            info.ForeColor = Color.White;
+            info.Location = new Point(43, 32);
+            info.Margin = new Padding(3, 2, 3, 2);
+            info.Name = "info";
+            info.Size = new Size(31, 26);
+            info.TabIndex = 16;
+            info.Text = "i";
+            info.UseVisualStyleBackColor = false;
+            info.Click += info_Click;
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.bggradient;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1664, 899);
+            Controls.Add(info);
+            Controls.Add(pictureBox8);
+            Controls.Add(togglemusicoff);
+            Controls.Add(togglemusicon);
+            Controls.Add(pictureBox6);
+            Controls.Add(gear);
+            Controls.Add(quit);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox5);
+            Controls.Add(statsbutton);
+            Controls.Add(freemodebutton);
+            Controls.Add(themebutton);
+            Controls.Add(pictureBox2);
+            Controls.Add(playbutton);
+            Controls.Add(pictureBox1);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox4);
+            DoubleBuffered = true;
+            Margin = new Padding(3, 2, 3, 2);
+            MaximizeBox = false;
+            Name = "FormMain";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)gear).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)togglemusicoff).EndInit();
+            ((System.ComponentModel.ISupportInitialize)togglemusicon).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button playbutton;
+        private PictureBox pictureBox1;
+        private Button themebutton;
+        private Button freemodebutton;
+        private Button statsbutton;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox gear;
+        private PictureBox pictureBox7;
+        private Button quit;
+        private PictureBox pictureBox6;
+        private PictureBox togglemusicoff;
+        private PictureBox togglemusicon;
+        private PictureBox pictureBox8;
+        private Button info;
+    }
+}
